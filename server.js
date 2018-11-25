f82ba4ecb6be0ac27f62844a0aaa1953b790fe9c
Tehseen <mt.tehseen@afiniti.com>
2018-11-25 21:56:39 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// app.use((req, res, next) => {
+//     var now = new Date().toString();
+//     console.log(`${now}: ${req.method} ${req.url}`);
+//     var log = `${now}: ${req.method} ${req.url}`;
+//     fs.appendFile('server.log', log + '\n', (err) =>{
+//         if(err){
+//             console.log('Unable to append to server.log');
+//         }
+//     })
+//     next();
+// });
+
+app.use((req, res, next) => {
+    res.render('maintainance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+// app.get('/', (req, res) => {
+//     // res.send('<h1>Hello World!</h1>');
+//     res.json({
+//         name: "tabish",
+//         likes: [
+//             "Cricket",
+//             "Football",
+//             "Hockey"
+//         ]
+//     });
+// });
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: "welcome to home page"
+        //currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        welcomeMessage: "welcome to about page"
+        //currentYear: new Date().getFullYear()
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Listening at port 3000');
+});
